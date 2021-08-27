4e0d4d5620166143f6abc55ceb93e43de03ffb52
Nika <kartozya.nika@yandex.ru>
2021-08-27 19:39:18 +0300

    test

---
+++ src/Complex.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace kartozianika\complex;
+
+use DivisionByZeroError;
+
+/**
+ * Комплексные числа
+ * Class Comlex
+ * @package kartozianika\complex
+ */
+abstract class Complex
+{
+    /**
+     * @var float
+     */
+    protected float $real;
+
+    /**
+     * @var float
+     */
+    protected float $imaginary;
+
+
+    /**
+     * Complex constructor.
+     * @param float $real
+     * @param float $imaginary
+     */
+    public function __construct(float $real, float $imaginary)
+    {
+        $this->real = $real;
+        $this->imaginary = $imaginary;
+    }
+
+    /**
+     * @param float $real
+     * @param float $imaginary
+     * @return Complex
+     */
+    public static function algebraic(float $real, float $imaginary): Complex
+    {
+        return new AlgebraicComplex($real, $imaginary);
+    }
+
+    /**
+     * @param float $module
+     * @param float $fi
+     * @return Complex
+     */
+    public static function trigonometric(float $module, float $fi): Complex
+    {
+        $real = $module * cos($fi);
+        $imaginary = $module * sin($fi);
+        return new TrigonometryComplex($real, $imaginary);
+    }
+
+    /**
+     * @return string
+     */
+    abstract public function __toString(): string;
+
+    /**
+     * Сложение
+     * @param Complex $b
+     * @return Complex
+     */
+    public function add(Complex $b): Complex
+    {
+        $new_real = $this->real + $b->getReal();
+        $new_imaginary = $this->imaginary + $b->getImaginary();
+
+        return new static($new_real, $new_imaginary);
+    }
+
+    /**
+     * Вычитание
+     * @param Complex $b
+     * @return Complex
+     */
+    public function subtract(Complex $b): Complex
+    {
+        $new_real = $this->real - $b->getReal();
+        $new_imaginary = $this->imaginary - $b->getImaginary();
+
+        return new static($new_real, $new_imaginary);
+    }
+
+    /**
+     * Умножение
+     * @param Complex $b
+     * @return Complex
+     */
+    public function multiply(Complex $b): Complex
+    {
+        $new_real = $this->real * $b->getReal() - $this->imaginary * $b->imaginary;
+        $new_imaginary = $this->imaginary * $b->getReal() + $this->real * $b->getImaginary();
+
+        return new static($new_real, $new_imaginary);
+    }
+
+    /**
+     * Деление
+     * @param Complex $b
+     * @return Complex
+     */
+    public function divide(Complex $b): Complex
+    {
+        if ($b->getReal() == 0 && $b->getImaginary() == 0) {
+            throw new DivisionByZeroError('Division by zero');
+
+        }
+
+        $bottom = $b->getReal() ** 2 + $b->getImaginary() ** 2;
+        $new_real = ($this->real * $b->getReal() + $this->imaginary * $b->getImaginary()) / $bottom;
+        $new_imaginary = ($this->imaginary * $b->getReal() - $this->real * $b->getImaginary()) / $bottom;
+
+        return new static($new_real, $new_imaginary);
+    }
+
+    /**
+     * @return float
+     */
+    public function getReal(): float
+    {
+        return $this->real;
+    }
+
+    /**
+     * @return float
+     */
+    public function getImaginary(): float
+    {
+        return $this->imaginary;
+    }
+}
---
+++ tests/ComlexTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace kartozianika\complex;
+
+use DivisionByZeroError;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Тесты для комплексных чисел
+ * Class ComlexTest
+ * @package kartozianika\complex
+ */
+class ComlexTest extends TestCase
+{
+
+    /**
+     * Тест сложения
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expect)
+    {
+        $addends = $a->add($b);
+        $this->assertEquals($expect, $addends);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [
+                Complex::algebraic(5, -6),
+                Complex::algebraic(-3, 2),
+                Complex::algebraic(2, -4),
+            ],
+            [
+                Complex::trigonometric(sqrt(2), pi() / 4),
+                Complex::trigonometric(sqrt(2), 3 * pi() / 4),
+                Complex::trigonometric(2, pi() / 2),
+            ],
+        ];
+    }
+
+    /**
+     * Тест вычитания
+     * @dataProvider subtractionProvider
+     */
+    public function testSubtract($a, $b, $expected)
+    {
+        $difference = $a->subtract($b);
+        $this->assertEquals($expected, $difference);
+    }
+
+    public function subtractionProvider()
+    {
+        return [
+            [
+                Complex::algebraic(5, -6),
+                Complex::algebraic(-3, 2),
+                Complex::algebraic(8, -8),
+            ],
+            [
+                Complex::trigonometric(3 * sqrt(2), pi() / 4),
+                Complex::trigonometric(2 * sqrt(2), pi() / 4),
+                Complex::trigonometric(sqrt(2), pi() / 4),
+            ],
+        ];
+    }
+
+    /**
+     * Тест умножения
+     * @dataProvider multiplierProvider
+     */
+    public function testMultiply($a, $b, $expected)
+    {
+        $product = $a->multiply($b);
+        $this->assertEquals($expected, $product);
+    }
+
+    public function multiplierProvider()
+    {
+        return [
+            [
+                Complex::algebraic(2, 3),
+                Complex::algebraic(-1, 1),
+                Complex::algebraic(-5, -1),
+            ],
+            [
+                Complex::trigonometric(1, pi() / 2),
+                Complex::trigonometric(2, pi() / 4),
+                Complex::trigonometric(2, 3 * pi() / 4),
+            ],
+        ];
+    }
+
+    /**
+     * Тест деления
+     * @dataProvider divisionProvider
+     */
+    public function testDivide($a, $b, $expected)
+    {
+        $quotient = $a->divide($b);
+        $this->assertEquals($expected, $quotient);
+    }
+
+    public function divisionProvider()
+    {
+        return [
+            [
+                Complex::algebraic(-2, 1),
+                Complex::algebraic(1, -1),
+                Complex::algebraic(-1.5, -0.5),
+            ],
+            [
+                Complex::trigonometric(2, 3 * pi() / 4),
+                Complex::trigonometric(1, pi() / 4),
+                Complex::trigonometric(2, pi() / 2),
+            ],
+        ];
+    }
+
+    /**
+     * Тест преобразования к строке
+     * @dataProvider stringifyProvider
+     */
+    public function testToString($a, $expected)
+    {
+        $this->assertEquals($expected, (string)$a);
+    }
+
+    public function stringifyProvider()
+    {
+        return [
+            [Complex::algebraic(1, 2), '1 + 2i'],
+            [Complex::algebraic(0, 2), '2i'],
+            [Complex::algebraic(1, 0), '1'],
+            [Complex::algebraic(0, 0), '0'],
+            [Complex::trigonometric(1, 3 * pi() / 4), 'cos(3PI/4) + i*sin(3PI/4)'],
+            [Complex::trigonometric(2, pi() / 2), '2(cos(PI/2) + i*sin(PI/2))'],
+        ];
+    }
+
+    /**
+     * Тест деления на нуль
+     */
+    public function testDivideZero()
+    {
+        $a = Complex::algebraic(123, 123);
+        $b = Complex::algebraic(0, 0);
+        $this->expectException(DivisionByZeroError::class);
+        $a->divide($b);
+    }
+}
